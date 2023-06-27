3bb99cdc72c3d76fd4df4caf1da5d3734b4f7027
eferreiradel <cont.limaferreira.everton@gmail.com>
2023-06-28 00:07:17 +0200

    --first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+@extends('template')
+
+@section('navbar-nav')
+    <a href="#">ITEN</a>
+    <a href="#">ITEN</a>
+    <a href="#">ITEN</a>
+    <a href="#">ITEN</a>
+@endsection
+
+@section('main')
+    <div class="container-fluid">
+        <div class="jumbotron">
+            <img src="{{ asset('images/jumbotron.jpg') }}" class="img-fluid jumbotron" alt="jumbotron">
+        </div>
+    </div>
+@endsection
+
